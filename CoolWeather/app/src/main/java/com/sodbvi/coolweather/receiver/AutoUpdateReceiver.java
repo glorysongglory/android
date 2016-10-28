380b36a14a8e01ba42a93c06963f2f5eb69afb81
sodbvi <317878410@qq.com>
2016-10-28 15:27:02 +0800

    exercise

---
+++ CoolWeather/app/src/main/java/com/sodbvi/coolweather/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,18 @@
+package com.sodbvi.coolweather.receiver;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.sodbvi.coolweather.service.AutoUpdateService;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i = new Intent(context, AutoUpdateService.class);
+		context.startService(i);
+	}
+
+}
